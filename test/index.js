a1abb55e1562ebc5987a5383188a3b5e04ff3f3c
jbellenger <jbellenger@twitter.com>
2017-05-19 11:15:16 -0700

    use let/const/arrows

--- test/index.js
+++ test/index.js
@@ -1,3 +1,3 @@
-var chai = require("chai");
-var chaiAsPromised = require("chai-as-promised");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
